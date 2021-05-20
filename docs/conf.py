4f02d3ab702ddc3be57fd3a2f3afbe66dca1b0c3
KhalilMayyasi <k.mayyasi@geidea.net>
2021-05-20 11:07:26 +0300

    fixed indentation problem

--- docs/conf.py
+++ docs/conf.py
@@ -261,7 +261,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
- rst_prolog = """
+rst_prolog = """
  .. include:: <s5defs.txt>
 
  """
