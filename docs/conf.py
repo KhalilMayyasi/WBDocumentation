fa7f824292833b474f83fd3d6aa725417e5d13ca
KhalilMayyasi <k.mayyasi@geidea.net>
2021-05-20 11:05:39 +0300

    adding a variable in the config to fix the styling issue

--- docs/conf.py
+++ docs/conf.py
@@ -260,3 +260,8 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+ rst_prolog = """
+ .. include:: <s5defs.txt>
+
+ """
